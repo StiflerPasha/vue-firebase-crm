4c90237a01c6f7a7c098818a2db536dda988dfef
StiflerPasha <dvoynospavel@gmail.com>
2019-10-16 13:38:40 +0300

    add layouts, views and config router

--- src/router.js
+++ src/router.js
@@ -11,7 +11,56 @@ export default new Router({
     {
       path: '/',
       name: 'home',
+      meta: { layout: 'main' },
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: { layout: 'empty' },
+      component: () => import('./views/Login.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      meta: { layout: 'empty' },
+      component: () => import('./views/Register.vue')
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      meta: { layout: 'main' },
+      component: () => import('./views/Categories.vue')
+    },
+    {
+      path: '/detail-record',
+      name: 'detail-record',
+      meta: { layout: 'main' },
+      component: () => import('./views/DetailRecord.vue')
+    },
+    {
+      path: '/history',
+      name: 'history',
+      meta: { layout: 'main' },
+      component: () => import('./views/History.vue')
+    },
+    {
+      path: '/planning',
+      name: 'planning',
+      meta: { layout: 'main' },
+      component: () => import('./views/Planning.vue')
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      meta: { layout: 'main' },
+      component: () => import('./views/Profile.vue')
+    },
+    {
+      path: '/record',
+      name: 'record',
+      meta: { layout: 'main' },
+      component: () => import('./views/Record.vue')
     },
   ]
 })
