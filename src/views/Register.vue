6dd56e8dea037e993061ed6538438b5735f24320
StiflerPasha <dvoynospavel@gmail.com>
2019-10-19 20:14:18 +0300

    add register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -87,7 +87,7 @@ export default {
     agree: { checked: v => v }
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch();
         return;
@@ -98,9 +98,11 @@ export default {
         password: this.password,
         name: this.name
       };
-      console.log(formData);
 
-      this.$router.push("/");
+      try {
+        await this.$store.dispatch("register", formData);
+        this.$router.push("/");
+      } catch (e) {}
     }
   }
 };
