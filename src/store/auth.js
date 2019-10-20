965915ec209c91fdb823a809300c8971de0e5093
StiflerPasha <dvoynospavel@gmail.com>
2019-10-20 14:42:27 +0300

    Update auth.js

--- src/store/auth.js
+++ src/store/auth.js
@@ -2,7 +2,6 @@ import firebase from 'firebase/app'
 
 export default {
    actions: {
-
       async login({ dispatch, commit }, { email, password }) {
          try {
             await firebase.auth().signInWithEmailAndPassword(email, password)
