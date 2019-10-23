d8d63ede5aaeca12c8faf773a3d685abe0190f03
StiflerPasha <dvoynospavel@gmail.com>
2019-10-23 19:17:38 +0300

    Update messages.js

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
    "logout": "Вы вышли из системы",
+   "login": "Для начала войдите в систему",
    "auth/user-not-found": 'Пользователь с таким Email не найден',
    "auth/wrong-password": "Неверный пароль",
    "auth/email-already-in-use": "Пользователь с таким Email уже существует"
